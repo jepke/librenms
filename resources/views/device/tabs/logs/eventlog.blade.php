d386854391755cfbd905ce1fa584f12888afa05c
Wheel <jepke@users.noreply.github.com>
2025-12-09 14:00:03 +0100

    Fix filter fields device event and syslog tabs

--- resources/views/device/tabs/logs/eventlog.blade.php
+++ resources/views/device/tabs/logs/eventlog.blade.php
@@ -18,10 +18,7 @@
                     </thead>
                 </table>
             </div>
-
-            @if(! $filter_device)
-                <input type="hidden" name="device" id="device" value="{{ $device->device_id }}">
-            @endif
+            <input type="hidden" name="device" id="device" value="{{ $device->device_id }}">
         </x-panel>
     </x-device.page>
 @endsection
@@ -65,13 +62,23 @@
             '</div>'
         );
 
-        init_select2("select#device", "device", {limit: 100}, "{{ $device->device_id }}");
-        init_select2("#eventtype", "eventlog", function(params) {
-            return {
-                field: "type",
-                device: $('#device').val(),
-                term: params.term,
-                page: params.page || 1
+        $('#eventtype').select2({
+            theme: 'bootstrap',
+            dropdownAutoWidth: true,
+            width: 'auto',
+            allowClear: true,
+            placeholder: 'All Types',
+            ajax: {
+                url: '{{ route('ajax.select.eventlog') }}',
+                delay: 200,
+                data: function (params) {
+                    return {
+                        field: 'type',
+                        device: $('#device').val(),
+                        term: params.term,
+                        page: params.page || 1
+                    };
+                }
             }
         }, @json($eventtype), 'All Types');
     </script>
--- resources/views/device/tabs/logs/syslog.blade.php
+++ resources/views/device/tabs/logs/syslog.blade.php
@@ -20,10 +20,7 @@
                     </thead>
                 </table>
             </div>
-
-            @if(! $filter_device)
-                <input type="hidden" name="device" id="device" value="{{ $device->device_id }}">
-            @endif
+            <input type="hidden" name="device" id="device" value="{{ $device->device_id }}">
         </x-panel>
     </x-device.page>
 @endsection
@@ -61,18 +58,22 @@
             '</div>' +
             '&nbsp;&nbsp;<div class="form-group">' +
             '<select name="program" id="program" class="form-control">' +
-                '<option value="">All Programs&nbsp;&nbsp;</option>' +
-            @if($program)
-                '<option value=\"' + @json($program) + '\">' + @json($program) + '</option>' +";
-            @endif
+                '<option value="">All Programs</option>' +
+                @if ($program)
+                    '<option value="' + @json($program) + '" selected>' +
+                        @json($program) +
+                    '</option>' +
+                @endif
             '</select>' +
             '</div>' +
             '&nbsp;&nbsp;<div class="form-group">' +
             '<select name="priority" id="priority" class="form-control">' +
                 '<option value="">All Priorities</option>' +
-            @if($priority)
-                '<option value=\"' + @json($priority) + '\">' + @json($priority) + '</option>' +";
-            @endif
+                @if ($priority)
+                    '<option value="' + @json($priority) + '" selected>' +
+                        @json($priority) +
+                    '</option>' +
+                @endif
             '</select>' +
             '</div>' +
             '&nbsp;&nbsp;<div class="form-group">' +
